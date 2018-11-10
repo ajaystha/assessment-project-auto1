076f4e56e5cc7c681da60ae672ee9e810616ca0a
Ajay Shrestha <azu.stha@yahoo.com>
2018-11-10 19:01:36 +0100

    update routes and layout styles

--- src/components/Layout/Toolbar/Navigation/Navigation.js
+++ src/components/Layout/Toolbar/Navigation/Navigation.js
@@ -6,7 +6,7 @@ import classes from './Navigation.module.scss';
 
 const navigation = () => (
   <ul className={classes.Navigation}>
-    <li><Link to="/404">Purchase</Link></li>
+    <li><Link to="/car-info">Purchase</Link></li>
     <li><Link to="/404">My Orders</Link></li>
     <li><Link to="/404">Sell</Link></li>
   </ul>
--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from  'react-router-dom';
 
 import MainView from '../containers/MainView/MainView';
+import CarDetails from '../components/CarDetails/CarDetails';
 import PageNotFound from '../components/PageNotFound/PageNotFound';
 
 
@@ -9,7 +10,7 @@ const router = () => (
   <Switch>
     <Route exact path="/" render={() => <Redirect to="/cars" />} />
     <Route path="/cars" component={MainView} />
-    <Route path="/cars/:id" component={MainView} />
+    <Route path="/car-info" component={CarDetails} />
     <Route component={PageNotFound} />
   </Switch>
 );
